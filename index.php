2c454b392d6177cd2659fa41f556d399fc73c37b
NagahamaTakahiro <nnarannnara27@gmail.com>
2021-05-06 08:16:03 +0900

    first-commit

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>琥珀LOG</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+    <!-- Third party plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="http://mysite02.local/wp-content/themes/KOHAblog/css/styles.css" rel="stylesheet" />
+    <?php wp_head(); ?>
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">琥珀LOG</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center justify-content-center text-center　font-family: Apple Chancery">
+                <div class="col-lg-10 align-self-end">
+                    <h1 class="text-uppercase text-white font-weight-bold font-family: Apple Chancery">初めまして、
+                    </h1>
+                    <h1 class="text-uppercase text-white font-weight-bold">横浜在住のWebエンジニアです。
+                    </h1>
+                    <p class="text-white-75 font-weight-light mb-5">横浜のWeb制作はお任せください。ご要望に添ったWebサイトを制作します。導線設計からコーディング、メンテナンスまで。</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- About-->
+    <section class="page-section bg-primary" id="about">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 text-center">
+                    <h2 class="text-white mt-0">初めまして、「たかひろ」といいます!</h2>
+                    <hr class="divider light my-4" />
+                    <img class="img-takahiro" src="http://mysite02.local/wp-content/uploads/2021/04/IMG_1479-e1619569956285.png" alt="red" style=Border-radius:100% />
+                    <p class="text-white-50 mb-4">Progate→ドットインストール→COACHTECH（オンラインスクール）を経てWeb制作をメインに活動しています！琥珀LOGの琥珀とは実家の猫のことです🐈💕
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Services-->
+    <section class="page-section" id="services">
+        <div class="container">
+            <h2 class="text-center mt-0">Service</h2>
+            <hr class="divider my-4" />
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fas fa-4x fa-laptop-house text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">HP制作</h3>
+                        <p class="text-muted mb-0">ホームページを制作します。</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">LP制作</h3>
+                        <p class="text-muted mb-0">ランディングページを制作します。</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fas fa-4x fa-award text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">バナー作成</h3>
+                        <p class="text-muted mb-0">集客につながるバナーを作成します。</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fas fa-4x fa-mobile-alt text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">レスポンシブ対応</h3>
+                        <p class="text-muted mb-0">スマホ、タブレット対応も可能。</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Skill-->
+    <section class="page-section" id="services">
+        <div class="container">
+            <h2 class="text-center mt-0">Skill</h2>
+            <hr class="divider my-4" />
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-4x fa-html5 text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">HTML</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-4x fa-css3-alt text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">CSS</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-4x fa-js-square text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">JavaScript</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-4x fa-vuejs text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">Vue.js</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-4x fa-php text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">PHP</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fas fa-4x fa-database text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">SQL</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-4x fa-laravel text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">Laravel</h3>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="mt-5">
+                        <i class="fab fa-4x fa-wordpress text-primary mb-4"></i>
+                        <h3 class="h4 mb-2">WordPress</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Portfolio-->
+    <section class="page-section bg-dark text-white">
+        <div id="portfolio">
+            <h2 class="text-center mt-0">Portfolio</h2>
+            <hr class="divider my-4" />
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="https://boring-golick-ab48a9.netlify.app">
+                            <img class="img-fluid" src=":assets/img/portfolio/thumbnails/1.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">デモサイト</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="https://nervous-einstein-8de07a.netlify.app">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">デモアプリ</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">ポートフォリオサイト</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- Contact-->
+    <section class="page-section" id="contact">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 text-center">
+                    <h2 class="mt-0">お問い合せにつながる</h2>
+                    <h2 class="mt-0">Webサイトを作ります！</h2>
+                    <hr class="divider my-4" />
+                    <p class="text-muted mb-5">横浜のWeb制作はお任せください。ご要望に添ったWebサイトを制作します。導線設計からコーディング、メンテナンスまで。</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                    <i class="fab fa-twitter fa-3x mb-3 text-muted"></i>
+                    <a class="d-block" href="https://twitter.com/takahiroFIRE1">@takahiroFIRE1</a>
+
+                </div>
+                <div class="col-lg-4 mr-auto text-center">
+                    <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                    <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                    <a class="d-block" href="mailto:nnarannnara27@gmail.com">nnarannnara27@gmail.com</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="bg-light py-5">
+        <div class="container">
+            <div class="small text-center text-muted">Copyright © 琥珀LOG.2021 </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
